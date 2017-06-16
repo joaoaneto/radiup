cca0ad457ecb9dccc68eaf88195d2acb46b7b14a
Matheus Felipe <matheus0906.mhci@gmail.com>
2017-06-15 22:57:50 -0300

    Creating server http
    Reviewers: joaoaneto, silvanoneto
    
    Reviewed By: joaoaneto, silvanoneto
    
    Subscribers: thiago-farias
    
    Maniphest Tasks: T59
    
    Differential Revision: https://phab.upelabs.com/D56

--- main.go
+++ main.go
@@ -1,28 +1,15 @@
 package main
 import (
 	"fmt"
-	"time"
-	"github.com/joaoaneto/radiup/cycle"
-	"github.com/joaoaneto/radiup/streamer"
-	"github.com/joaoaneto/radiup/playlist"
-	"github.com/joaoaneto/radiup/dbconf"
+	"net/http"
 )
 
 func main(){
 	// Declarations without meaning only for build the main.go
 
-	var music []cycle.Music
-	var cycle1 cycle.Cycle
-
-	usuario := cycle.User{"Usuario", "usuario", "123456", time.Date(2009, time.November, 10, 23, 0, 0, 0, time.UTC), "usuario@gmail.com", 'M'}
-	fmt.Println(usuario)
-
-	streamer := streamer.Streamer{"Streamer"}
-	fmt.Println(streamer)
-
-	playlist := playlist.Playlist{1, music, cycle1}
-	fmt.Println(playlist)
-
-	fmt.Print(dbconf.GetConnectionData().Database)
-
+	/*Especified de url of the service*/
+	http.HandleFunc("/radiup", func(w http.ResponseWriter, r *http.Request){
+			fmt.Fprintf(w, "Testando")
+		})
+	http.ListenAndServe(":8080", nil)
 }
