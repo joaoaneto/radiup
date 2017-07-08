2d7672a20809353247c8cffc6283e87d75452bdb
Lucas FeijÃ³ Portela <lucasfjportela@gmail.com>
2017-07-08 11:21:02 -0300

    (Form de Registro) Redirecionamento para a pagina de Login no Spotify
    Reviewers: joaoaneto, Matheus-felipe
    
    Reviewed By: joaoaneto
    
    Subscribers: thiago-farias
    
    Maniphest Tasks: T49
    
    Differential Revision: https://phab.upelabs.com/D76

--- controller/register.go
+++ controller/register.go
@@ -4,10 +4,17 @@ import (
 	"html/template"
 	"net/http"
 	"path"
+	"github.com/joaoaneto/radiup/streamer"
+	"fmt"
 )
 
 func RegisterHandler(w http.ResponseWriter, r *http.Request) {
 
+	spotifyStreamer := streamer.GetStreamerManager().Get("SPOTIFY")
+	spotifyStreamer.AuthRPC.NewAuthenticator("http://localhost:8080/callback")
+	oAuthTest := streamer.OAuthInfo{ClientID: "42d13a4cacae480189b2702e48d6879a", SecretKey: "f0864a30cca443c4b33b940940285d87"}
+	spotifyStreamer.AuthRPC.SetAuthInfo(oAuthTest)
+	
 	fp1 := path.Join("templates", "base.html")
 	fp2 := path.Join("templates", "register_form.html")
 	t, err := template.ParseFiles(fp1, fp2)
@@ -17,9 +24,11 @@ func RegisterHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// Get URL
+	templateUrl := spotifyStreamer.AuthRPC.GetAuthURL()
+
 	// send any data for template render through second argument in t.Execute()
-	if err := t.Execute(w, nil); err != nil {
+	if err := t.Execute(w, templateUrl); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
-
 }
