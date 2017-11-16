d50ba1edb3f0bfafecce35cc516581a2c91905f2
Lucas Feijó Portela <lucasfjportela@gmail.com>
2017-11-16 13:53:16 -0300

    Implementação do endpoint da API de Cycle: /suggest/music

--- cycle/service/handlers.go
+++ cycle/service/handlers.go
@@ -1,1 +1,51 @@
 package service
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+
+	"time"
+
+	"github.com/gorilla/mux"
+	"golang.org/x/crypto/bcrypt"
+
+	"github.com/joaoaneto/radiup/cycle"
+	"github.com/joaoaneto/radiup/cycle/model"
+	"github.com/joaoaneto/radiup/cycle/repository"
+)
+
+var Db *repository.MySQLConfig
+
+func GetVoluntarySuggestionsHandler(w http.ResponseWriter, r *http.Request) {
+
+	/* Get user in db
+	var userId = mux.Vars(r)["userId"]
+
+	sup := repository.NewSimpleUserPersistor(Db)
+	*/
+	var localCycle cycle.Cycle{0, nil, nil, nil, nil, nil, nil, nil}
+
+	cp := repository.NewCyclePersistor(Db)	
+	cp.Create(localCycle)
+
+	listVS, err := cp.SearchAll(localCycle.ID)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	data, err := json.Marshal(listVS)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-type", "application/json")
+	w.Header().Set("Content-Length", strconv.Itoa(len(data)))
+	w.WriteHeader(http.StatusOK)
+	w.Write(data)
+
+}
